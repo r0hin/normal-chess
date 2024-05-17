63b5b3fb414a1199a6b0dbf4725c558c35dda3e9
Rohin <git@r0h.in>
2024-05-17 18:17:02 -0400

    Complete preprocessing

--- tests/preprocessing.py
+++ tests/preprocessing.py
@@ -28,52 +28,6 @@ def auto_canny(image, sigma=0.33):
     # return the edged image
     return edged
 
-def cluster(points, max_dist=50):
-    """
-    Given a list of points, returns a list of cluster centers.
-    """
-    Y = spatial.distance.pdist(points)
-    Z = clstr.hierarchy.single(Y)
-    T = clstr.hierarchy.fcluster(Z, max_dist, 'distance')
-    clusters = defaultdict(list)
-    for i in range(len(T)):
-        clusters[T[i]].append(points[i])
-    clusters = clusters.values()
-    clusters = map(lambda arr: (np.mean(np.array(arr)[:,0]), np.mean(np.array(arr)[:,1])), clusters)
-    return clusters
-
-def closest_point(points, loc):
-    """
-    Returns the list of points, sorted by distance from loc.
-    """
-    dists = np.array(map(partial(spatial.distance.euclidean, loc), points))
-    return points[dists.argmin()]
-
-def find_corners(points, img_dim):
-    """
-    Given a list of points, returns a list containing the four corner points.
-    """
-    center_point = closest_point(points, (img_dim[0] / 2, img_dim[1] / 2))
-    points.remove(center_point)
-    center_adjacent_point = closest_point(points, center_point)
-    points.append(center_point)
-    grid_dist = spatial.distance.euclidean(np.array(center_point), np.array(center_adjacent_point))
-    
-    img_corners = [(0, 0), (0, img_dim[1]), img_dim, (img_dim[0], 0)]
-    board_corners = []
-    tolerance = 0.25 # bigger = more tolerance
-    for img_corner in img_corners:
-        while True:
-            cand_board_corner = closest_point(points, img_corner)
-            points.remove(cand_board_corner)
-            cand_board_corner_adjacent = closest_point(points, cand_board_corner)
-            corner_grid_dist = spatial.distance.euclidean(np.array(cand_board_corner), np.array(cand_board_corner_adjacent))
-            if corner_grid_dist > (1 - tolerance) * grid_dist and corner_grid_dist < (1 + tolerance) * grid_dist:
-                points.append(cand_board_corner)
-                board_corners.append(cand_board_corner)
-                break
-    return board_corners
-
 def four_point_transform(img, points, square_length=1816):
     pts1 = np.float32(points)
     pts2 = np.float32([[0, 0], [0, square_length], [square_length, square_length], [square_length, 0]])
@@ -82,6 +36,7 @@ def four_point_transform(img, points, square_length=1816):
 
 def find_board(fname):
     img = cv2.imdecode(fname, 1)
+    original_img = img
     if img is None:
         return [None, "no image"]
 
@@ -391,66 +346,65 @@ def find_board(fname):
 
     cv2.imwrite('hough_lines_on_image.jpg', img)
     
-    # Compute intersection points
-    # h, v = hor_vert_lines(lines)
-    # if len(h) < 9 or len(v) < 9:
-    #     return [None, "too few lines"]
-    # points = intersections(h, v)
-    
-    # # Cluster intersection points
-    # points = cluster(points)
-
-    # # Find corners
-    # img_shape = np.shape(img)
-    # points = find_corners(points, (img_shape[1], img_shape[0]))
-    
-    # # Perspective transform
-    # new_img = four_point_transform(img, points)
-
-    # # write new image
-    # cv2.imwrite('board.jpg', new_img)
-
-    # return new_img
-    exit()
-    return 0
-
-def hor_vert_lines(lines):
-    """
-    A line is given by rho and theta. Given a list of lines, returns a list of
-    horizontal lines (theta=90 deg) and a list of vertical lines (theta=0 deg).
-    """
-    h = []
-    v = []
-    for distance, angle in lines:
-        if angle < np.pi / 4 or angle > np.pi - np.pi / 4:
-            v.append([distance, angle])
-        else:
-            h.append([distance, angle])
-    return h, v
-
-def intersections(h, v):
-    """
-    Given lists of horizontal and vertical lines in (rho, theta) form, returns list
-    of (x, y) intersection points.
-    """
     points = []
-    for d1, a1 in h:
-        for d2, a2 in v:
+    for d1, a1 in horizontal_lines:
+        for d2, a2 in vertical_lines:
             A = np.array([[np.cos(a1), np.sin(a1)], [np.cos(a2), np.sin(a2)]])
             b = np.array([d1, d2])
             point = np.linalg.solve(A, b)
             points.append(point)
-    return np.array(points)
+    
+    points = np.array(points)
+
+    # At each point, draw a big purple circle with a radius of 15 pixels
+    for point in points:
+        cv2.circle(img, (int(point[0]), int(point[1])), 15, (255, 0, 255), -1)
+
+    # given a collection of points, find the four corners of the quadrilateral that encloses them
+    def find_corners(points):
+        hull = spatial.ConvexHull(points)
+        hull_points = points[hull.vertices]
+        return hull_points
+    
+    corners = find_corners(points)
+    for corner in corners:
+        cv2.circle(img, (int(corner[0]), int(corner[1])), 15, (0, 0, 255), -1)
+
+    # Tranform "points" by the same perspective transformation 
+    new_img = four_point_transform(original_img, corners)
+    cv2.imwrite('board.jpg', new_img)
+
+    squared = split_board(new_img)
+
+    for i in range(64):
+        print('writing square', i)
+        cv2.imwrite('square' + str(i) + '.jpg', squared[i])
+
+
+
+    # # Perspective transform
+
+    # # write new image
+
+    # return new_img
+    exit()
+    return 0
 
 def split_board(img):
     """
     Given a board image, returns an array of 64 smaller images.
     """
     arr = []
-    sq_len = img.shape[0] / 8
+    sq_len = img.shape[0] // 8  # Use integer division to get an integer result
     for i in range(8):
         for j in range(8):
-            arr.append(img[i * sq_len : (i + 1) * sq_len, j * sq_len : (j + 1) * sq_len])
+            # Calculate the start and end indices for each small square
+            start_row = i * sq_len
+            end_row = (i + 1) * sq_len
+            start_col = j * sq_len
+            end_col = (j + 1) * sq_len
+            # Append the cropped section to the array
+            arr.append(img[start_row:end_row, start_col:end_col])
     return arr
 
 if __name__ == '__main__':
@@ -458,9 +412,3 @@ if __name__ == '__main__':
     file = open('/Users/rohin/Desktop/IMG_8452.jpg', 'rb')
     img = np.asarray(bytearray(file.read()))
     boards = find_board(img)
-    board = boards[0]
-    reason = boards[1]
-    if board is None:
-        print(reason)
-    squares = split_board(board)
-    print('finished board rec')
